c89a98d89f0875d6e59a30813f31692ba59d1724
Pierre <pierrej0312@gmail.com>
2020-03-27 16:55:29 +0100

    smooth scroll

--- animous.js
+++ animous.js
@@ -6,30 +6,13 @@ window.addEventListener("load", function() {
     logo.classList.add("loaded");
 });
 
-
-//smooth scrolling
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
-
-for (let item of anchorlinks) { // relitere 
-    item.addEventListener('click', (e)=> {
-        let hashval = item.getAttribute('href')
-        let target = document.querySelector(hashval)
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-        history.pushState(null, null, hashval)
-        e.preventDefault()
-    })
-}
-
 //nav bar desktop
 
 window.addEventListener("scroll", ()=> {
     if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.querySelector("header").classList.remove(start);
+        document.querySelector("header").classList.remove("start");
       } 
     else {
-        document.querySelector("header").classList.add(start);
+        document.querySelector("header").classList.add("start");
     }
 });
